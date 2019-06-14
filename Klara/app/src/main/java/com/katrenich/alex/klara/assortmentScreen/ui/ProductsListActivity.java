1b0393c60d6f0d7d1b6f6b228ad4d18fde3e264b
AlexKatrenich <alex.katrenich@gmail.com>
2019-06-14 12:39:36 +0300

    changed toolbar on collapsing toolbar

--- Klara/app/src/main/java/com/katrenich/alex/klara/assortmentScreen/ui/ProductsListActivity.java
+++ Klara/app/src/main/java/com/katrenich/alex/klara/assortmentScreen/ui/ProductsListActivity.java
@@ -1,16 +1,39 @@
 package com.katrenich.alex.klara.assortmentScreen.ui;
 
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.katrenich.alex.klara.R;
+import com.katrenich.alex.klara.assortmentScreen.viewmodel.ProductListActivityViewModel;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ProductsListActivity extends AppCompatActivity {
+    @BindView(R.id.tb_activity_product_list)
+    protected Toolbar mToolbar;
+
+    private ProductListActivityViewModel mViewModel;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products_list);
+        ButterKnife.bind(this);
+        init(savedInstanceState);
+    }
+
+    private void init(Bundle savedInstanceState) {
+        mViewModel = ViewModelProviders.of(this).get(ProductListActivityViewModel.class);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        mToolbar.setNavigationOnClickListener(v -> onBackPressed());
     }
 }
---
+++ Klara/app/src/main/java/com/katrenich/alex/klara/assortmentScreen/viewmodel/ProductListActivityViewModel.java
@@ -0,0 +1,7 @@
+package com.katrenich.alex.klara.assortmentScreen.viewmodel;
+
+import android.arch.lifecycle.ViewModel;
+
+public class ProductListActivityViewModel extends ViewModel {
+
+}
